873bd7ede0eb2ce2093331f5ac5331b7243b2c45
Ryusuke Nakamura <ryusukwww@gmail.com>
2021-01-14 22:13:19 +0900

    publicpathを変更

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    publicPath: './',
+    publicPath: '/portfolio/',
     outputDir: 'docs',
     filenameHashing: false,
     productionSourceMap: false,
