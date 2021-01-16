761b79141787a6889f098f4aa3e2971db6e08e2f
Ryusuke Nakamura <ryusukwww@gmail.com>
2021-01-16 15:32:07 +0900

    タイトル画像入れ替え

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="uk-flex uk-flex-center uk-flex-middle uk-background-cover " uk-height-viewport="expand: true" data-src="images/-back-ground.png" uk-img>
-        <div class="uk-text-center uk-margin-large">
+        <div class="uk-text-center uk-margin-large uk-margin-xlarge-right uk-margin-xlarge-left">
             <p><img class="uk-border-circle" src="@/assets/pofile.jpg" width="150" uk-img></p>
-            <p>はじめまして！中村颯介と申します。</p> 
-            <p>WordPressを主軸に、企画・デザイン・コーディングを行なっています。</p> 
-            <p>現在はモダンなエンジニアリングを学ぶため、javascriptを学んでいます</p> 
+            <img src="@/assets/name.png" width="400" height="200" alt="">
+            <p>はじめまして！中村颯介（なかむらりゅうすけ）と申します。</p>
+            <p>現在は奈良県にあるサッカーチームに所属し、Web企画・デザイン・コーディングを行なっています。</p>
+            <p>趣味はフットサルと漫画を読む事です。よろしくお願いします。</p> 
             <span class="uk-margin-small-right uk-margin-samll-bottom uk-label uk-sort-label">WORDPRESS 3年</span>
             <span class="uk-margin-small-right uk-margin-samll-bottom uk-label uk-sort-label">HTML/CSS 3年</span>
             <span class="uk-margin-small-right uk-margin-samll-bottom uk-label uk-sort-label">UIKIT 1年</span>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -8,7 +8,7 @@
         <!--カード1-->
         <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
             <div class="uk-card-media-left uk-cover-container">
-                <img src="@/assets/naraclub.jpeg" alt="" uk-cover>
+                <img src="@/assets/naraclub.png" alt="" uk-cover>
                 <canvas width="600" height="400"></canvas>
             </div>
             <div>
@@ -34,7 +34,7 @@
                     <p>勤続年数 : 2015年12月 - 2019年8月</p>
                     <p>所属部署 : プロジェクトマネジャー、Webデザイン、Webコーディング</p>
                     <p>業務内容 : マネジメント、Webデザイン、コーディング</p>
-                    <p>IT支援及びITのオンライン学習体験サービスの提供を行う。プロジェクトマネジャーとしてユーザーのKPI設計と評価システム作成、現場の進捗管理を行いながら専用システムの構築・運用も行う。</p>
+                    <p>WordPressを中心としたIT教育事業に従事。教育事業のUX/UI設計、コーディング、チームのマネジメント業務を行う。</p>
                     <a href="https://toiee.jp/" class="uk-button uk-button-text" target="_blank" rel="noopener noreferrer"></a>
                 </div>
             </div>
